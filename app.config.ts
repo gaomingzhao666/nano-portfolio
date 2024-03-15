9a30cfc594a351cd160d60e7eeac03fe9ec81730
nano <gaomingzhao666@outlook.com>
2024-03-15 18:15:54 +0800

    ⚡ init

--- app.config.ts
+++ app.config.ts
@@ -1,9 +1,14 @@
 export default defineAppConfig({
-	ui: {
-		primary: 'blue',
-		gray: 'cool',
-		// container: {
-		// 	constrained: 'max-w-8xl',
-		// },
-	},
-})
+  ui: {
+    primary: "blue",
+    gray: "cool",
+    // container: {
+    // 	constrained: 'max-w-8xl',
+    // },
+
+    //make nuxt-icon(namely iconify) available
+    icons: {
+      dynamic: true,
+    },
+  },
+});
