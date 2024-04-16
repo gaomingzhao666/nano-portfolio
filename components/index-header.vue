08ade8d43df22b5229c51bae0889655d683dbbfd
nano <gaomingzhao666@outlook.com>
2024-04-16 20:36:21 +0800

    ⚡: add link to login page

--- components/index-header.vue
+++ components/index-header.vue
@@ -1,37 +1,35 @@
 <template>
-  <header class="flex justify-between items-center m-5 border-b-gray-700">
-    <div class="flex">
-      <ico-nano class="text-5xl rounded-full" />
+	<header class="flex justify-between items-center m-5 border-b-gray-700">
+		<div class="flex">
+			<ico-nano class="text-5xl rounded-full" />
 
-      <div class="ml-3">
-        <p class="text-blue-600 text-lg">
-          Nano
-        </p>
-        <p class="text-gray-500 text-sm">
-          Front-end developer
-        </p>
-      </div>
-    </div>
+			<div class="ml-3">
+				<p class="text-blue-600 text-lg">Nano</p>
+				<p class="text-gray-500 text-sm">Front-end developer</p>
+			</div>
+		</div>
 
-    <UInput
-      icon="i-mingcute:search-line"
-      size="lg"
-      color="white"
-      trailing
-      placeholder="Search projects"
-      class="w-1/3"
-    />
+		<UInput
+			icon="i-mingcute:search-line"
+			size="lg"
+			color="white"
+			trailing
+			placeholder="Search projects"
+			class="w-1/3"
+		/>
 
-    <UButton
-      icon="i-material-symbols:login"
-      color="white"
-      variant="solid"
-      size="md"
-    >
-      Login
-    </UButton>
-  </header>
-  <UDivider />
+		<NuxtLink to="/login">
+			<UButton
+				icon="i-material-symbols:login"
+				color="white"
+				variant="solid"
+				size="md"
+			>
+				Login
+			</UButton>
+		</NuxtLink>
+	</header>
+	<UDivider />
 </template>
 
 <script lang="ts" setup></script>
