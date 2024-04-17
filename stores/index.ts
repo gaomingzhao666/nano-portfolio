4fa7aef796485e634435120d9bee000b5813ff25
nano <gaomingzhao666@outlook.com>
2024-04-17 21:30:51 +0800

    ⚙: make file condense

--- stores/index.ts
+++ stores/index.ts
@@ -1,10 +1,3 @@
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const name = ref('Eduardo')
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
-  }
-
-  return { count, name, doubleCount, increment }
+export const useStore = defineStore('store', () => {
+	// ---
 })
