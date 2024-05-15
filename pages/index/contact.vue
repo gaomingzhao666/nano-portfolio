24ef849e3fd724a15fe2c6d58975c09e52147563
nano <gaomingzhao666@outlook.com>
2024-05-15 21:13:35 +0800

    ⚡: added contact page

---
+++ pages/index/contact.vue
@@ -0,0 +1,21 @@
+<template>
+	<UContainer>
+		<h1 class="text-center font-bold text-3xl my-10">Contact me</h1>
+		<section class="shadow rounded-3xl border-gray-100 my-6 p-5">
+			<section class="flex items-center m-3">
+				<ico-phone class="text-3xl mr-3" />
+				<p>+86 13244687353</p>
+			</section>
+			<section class="flex items-center m-3">
+				<ico-email class="text-3xl mr-3" />
+				<p>gaomingzhao666@outlook.com</p>
+			</section>
+			<section class="flex items-center m-3">
+				<ico-address class="text-3xl mr-3" />
+				<p>Earth</p>
+			</section>
+		</section>
+	</UContainer>
+</template>
+
+<script lang="ts" setup></script>
