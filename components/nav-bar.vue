38dadbbfeb221903262cde476a0317dcfa2886f9
nano <gaomingzhao666@outlook.com>
2024-05-28 21:26:35 +0800

    ⚡: modularize navigation data

--- components/nav-bar.vue
+++ components/nav-bar.vue
@@ -1,47 +1,18 @@
 <template>
-  <nav class="flex m-3 h-full">
-    <UVerticalNavigation
-      :links="links"
-      class="h-full"
-      :ui="{
-        padding: 'p-5',
-        width: 'w-64',
-        rounded: 'rounded-lg',
-      }"
-    />
-    <UDivider
-      orientation="vertical"
-      class="m-3 h-full"
-    />
-  </nav>
+	<nav class="flex m-3 h-full">
+		<UVerticalNavigation
+			:links="navbar.links"
+			class="h-full"
+			:ui="{
+				padding: 'p-5',
+				width: 'w-64',
+				rounded: 'rounded-lg',
+			}"
+		/>
+		<UDivider orientation="vertical" class="m-3 h-full" />
+	</nav>
 </template>
 
 <script lang="ts" setup>
-const route = useRoute()
-
-const links = [
-  {
-    label: 'explore',
-    icon: 'i-heroicons-home',
-    to: '/getting-started/installation'
-  }, {
-    label: 'experience',
-    icon: 'i-heroicons-chart-bar',
-    to: `${route.path.startsWith('/dev') ? '/dev' : ''}/components/vertical-navigation`
-  }, {
-    label: 'Command Palette',
-    icon: 'i-heroicons-command-line',
-    to: '/components/command-palette'
-  },
-  {
-    label: 'Command Palette',
-    icon: 'i-heroicons-command-line',
-    to: '/components/command-palette'
-  },
-   {
-    label: 'explore',
-    icon: 'i-heroicons-home',
-    to: '/getting-started/installation'
-  },
-]
+const navbar = useNavbarStore()
 </script>
