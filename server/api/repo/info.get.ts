44903a3df64a3773e36babcd66c1ecd34598d440
nano <gaomingzhao666@outlook.com>
2024-05-28 11:01:30 +0800

    ⚡: block useless repos in server-side

--- server/api/repo/info.get.ts
+++ server/api/repo/info.get.ts
@@ -1,6 +1,31 @@
 import { Octokit } from 'octokit'
 import { githubToken } from '~/server/utils/githubInfo'
 
+// repoImage url consist by `~/assets/images/repoImages/${repo}Image`
+// repoImages:[string]
+const baseUrl: string = '~/assets/images/repoImages'
+const suffix: string = 'Image'
+
+const getRepoIamgeUrl = (repoInfo: any) => {
+	let repoName: [string] = ['']
+	for (let index: number = 0; index < repoInfo.data.length; index++)
+		repoName[index] = repoInfo.data[index].name
+	const repoImageUrl: [string] = ['']
+	for (let index: number = 0; index < repoName.length; index++) {
+		repoImageUrl[index] = `${baseUrl}/${repoName[index]}/${suffix}`
+	}
+	return <[string]>repoImageUrl
+}
+
+const getClearedRepo = (repoInfo: any) => {
+	console.log(repoInfo.data.topics)
+
+	return repoInfo.data.filter(
+		(data: any) =>
+			!(data.topics.includes('config') || data.topics.includes('backup'))
+	)
+}
+
 export default defineEventHandler(async (event) => {
 	try {
 		const getGithubRepoInfo = async () => {
@@ -16,12 +41,19 @@ export default defineEventHandler(async (event) => {
 			return data
 		}
 
-		const githubRepoInfo = await getGithubRepoInfo()
+		let githubRepoInfo = await getGithubRepoInfo()
+		console.log(githubRepoInfo)
+
+		const repoImageUrl: [string] = getRepoIamgeUrl(githubRepoInfo)
+		const newGithubRepoInfo = getClearedRepo(githubRepoInfo)
+		console.log(newGithubRepoInfo)
+
 		if (githubRepoInfo.status === 200) {
 			setResponseStatus(event, 200)
 			return <repoInfoGet>{
 				status: true,
-				data: githubRepoInfo,
+				data: newGithubRepoInfo,
+				repoImageUrl: repoImageUrl,
 			}
 		} else
 			return <errorType>{
