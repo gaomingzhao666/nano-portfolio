1ae1c0301e57d748997c04543370723e88d309bc
nano <gaomingzhao666@outlook.com>
2024-05-29 18:17:06 +0800

    ⚡: init component

---
+++ components/page-explain.vue
@@ -0,0 +1,14 @@
+<template>
+	<section>
+		<h1>{{ props.message }}</h1>
+	</section>
+</template>
+
+<script lang="ts" setup>
+interface props {
+	message: string
+}
+const props = defineProps<props>()
+</script>
+
+<style></style>
