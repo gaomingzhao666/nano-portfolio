cc9b0e875b873afdbecb3156eb170d427fe51a89
nano <gaomingzhao666@outlook.com>
2024-05-29 18:16:52 +0800

    ⚡: update info of github account

--- server/utils/githubInfo.ts
+++ server/utils/githubInfo.ts
@@ -1,4 +1,4 @@
 export const githubToken: string =
-	'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'
+	'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'
 
 export const githubName: string = 'gaomingzhao666'
