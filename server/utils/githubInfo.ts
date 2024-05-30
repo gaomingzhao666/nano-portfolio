8712d20fb903ed47f2d5a968cbf7e166c6b3694d
gaomingzhao666 <gaomingzhao666@outlook.com>
2024-05-30 15:11:48 +0800

    test whether token is expire when push token to repo

--- server/utils/githubInfo.ts
+++ server/utils/githubInfo.ts
@@ -1,4 +1,5 @@
 export const githubToken: string =
-	'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'
+	'github' +
+	'_pat_11BFA2Z7Q0h8kbZ60qYKYZ_JkK3HkORHYAl6NfFVp955NMCOJ9UeUugbl7X6U4ij3CWBPLLS6G1oALkTrS'
 
 export const githubName: string = 'gaomingzhao666'
