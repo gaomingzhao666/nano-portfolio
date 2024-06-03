5afc74005fd13862685856a8238f05b604841291
Nano <155299070+gaomingzhao666@users.noreply.github.com>
2024-06-03 19:18:46 +0800

    ⚡: add http loading indicator

--- app.vue
+++ app.vue
@@ -1,10 +1,12 @@
 <template>
 	<div>
 		<NuxtLayout>
-			<NuxtPage />
-		</NuxtLayout>
+			<NuxtLoadingIndicator />
 
-		<UNotifications />
+			<NuxtPage />
+
+			<UNotifications />
+		</NuxtLayout>
 	</div>
 </template>
 
@@ -12,11 +14,19 @@
 useSeoMeta({
 	title: 'nano-portfolio',
 	description:
-		'Nano-portfolio is a developer introduction web application integrated with Github Octokit and Nuxt3 ecosystem that includes Nuxt-UI  Nuxt-Mongoose Pinia i18n etc. All of the modules are officially compatible with Nuxt3, and all of the code inside of this project uses composition API and ESM.',
+		'Nano-portfolio is a full-stack and open-source web application for developer introduction integrated with Github Octokit and Nuxt3 ecosystem that includes Nuxt-UI Nuxt-Mongoose Pinia i18n etc. All of the modules are officially compatible with Nuxt3, and all of the code inside of this project uses composition API and ESM.',
+})
+
+const { progress, isLoading, start, finish, clear } = useLoadingIndicator({
+	duration: 2000,
+	throttle: 200,
+	// This is how progress is calculated by default
+	estimatedProgress: (duration, elapsed) =>
+		(2 / Math.PI) * 100 * Math.atan(((elapsed / duration) * 100) / 50),
 })
 </script>
 
-<style>
+<style scoped>
 .page-enter-active,
 .page-leave-active {
 	transition: all 0.4s;
