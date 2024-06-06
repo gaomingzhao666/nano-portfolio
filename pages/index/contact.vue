4a349a7fc0c79d4fb7a1b4c1d957eb68241d5c62
Nano <155299070+gaomingzhao666@users.noreply.github.com>
2024-06-06 11:14:37 +0800

    ⚡: add links about me

--- pages/index/contact.vue
+++ pages/index/contact.vue
@@ -3,6 +3,7 @@
 		<h1 class="text-center font-bold text-3xl my-10">
 			{{ $t('contactMe') }}
 		</h1>
+
 		<section class="shadow rounded-3xl border-gray-100 my-6 p-5">
 			<section class="flex items-center m-5">
 				<ico-phone class="text-3xl mr-3" />
@@ -16,6 +17,17 @@
 				<ico-address class="text-3xl mr-3" />
 				<p>{{ $t('earth') }}</p>
 			</section>
+		</section>
+
+		<section class="text-center shadow rounded-3xl border-gray-100 my-6 p-5">
+			<a href="https://github.com/gaomingzhao666">
+				<UButton
+					icon="i-grommet-icons:github"
+					size="sm"
+					color="black"
+					class="rounded-full"
+				/>
+			</a>
 		</section>
 	</UContainer>
 </template>
