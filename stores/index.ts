d01a4b327608ede07f8a249121856fd7d82d8f04
Nano <155299070+gaomingzhao666@users.noreply.github.com>
2024-06-22 11:05:10 +0800

    ⚡: delete useless files and codes

--- stores/index.ts
+++ stores/index.ts
@@ -1,4 +1,1 @@
-export const useTokenStore = defineStore('token', () => {
-	// const token: Ref<string | null> = ref(localStorage.getItem('token'))
-	// const getToken = computed(() => token)
-})
+// Pinia index
--- utils/delayTime.ts
+++
@@ -1,1 +0,0 @@
-export default (): number => 1500
