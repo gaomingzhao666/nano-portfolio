1711554c876a677b438b634e212a2d38704a885e
Nano <155299070+gaomingzhao666@users.noreply.github.com>
2024-06-27 11:28:24 +0800

    ⚡: fill comments page logic and UI

---
+++ utils/getLocalTime.ts
@@ -0,0 +1,7 @@
+export const getLocalTime = (databaseTime: string): string => {
+	const date: Date = new Date(databaseTime)
+	const localizedTime: string = date.toLocaleTimeString()
+	const localizedDate: string = date.toLocaleDateString()
+
+	return `${localizedDate} ${localizedTime}`
+}
