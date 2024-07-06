390618a09f8369ca76d9b73560dd973cfb24fec5
Nano <155299070+gaomingzhao666@users.noreply.github.com>
2024-07-06 16:54:42 +0800

    ⚡:  integrate mongodb with vercel

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,7 +23,7 @@ export default defineNuxtConfig({
 	},
 
 	mongoose: {
-		uri: 'mongodb://localhost:27017/nano-portfolio',
+		uri: 'mongodb+srv://gaomingzhao:<Gmz020413>@gaomingzhao.skelvi3.mongodb.net/?appName=gaomingzhao',
 		options: {},
 	},
 
