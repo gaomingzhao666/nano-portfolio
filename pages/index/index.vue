ab915a4f2ec042a93166d666d50144ac2c236373
Nano <155299070+gaomingzhao666@users.noreply.github.com>
2024-07-07 14:33:55 +0800

    ⚙: responsive fixes to sm size of screen

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,5 +1,5 @@
 <template>
-	<main class="grid 2xl:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-3">
+	<main class="grid 2xl:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-3 mx-auto">
 		<section
 			class="group rounded-3xl shadow-md m-3 hover:shadow-lg transition-all"
 			v-for="(item, index) in data?.data"
