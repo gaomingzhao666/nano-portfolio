6c1caec43fa189ec7c8d83539d84991b09b2bae7
Nano <155299070+gaomingzhao666@users.noreply.github.com>
2024-07-10 12:06:22 +0800

    fix: modularize repoCard component

---
+++ components/repoCard.vue
@@ -0,0 +1,57 @@
+<template>
+	<UCard
+		class="group rounded-3xl m-3 hover:shadow-lg transition-all"
+		:ui="{ body: { padding: '' } }"
+	>
+		<section
+			class="relative text-white bg-slate-700 w-full min-h-48 p-5 rounded-3xl rounded-b-none"
+		>
+			<section class="flex justify-start items-center">
+				<UIcon name="i-grommet-icons:github" class="text-3xl mr-2" />
+				<p>{{ $t('githubRepo') }}</p>
+			</section>
+
+			<h1
+				class="text-2xl text-center my-10 transition-all group-hover:text-3xl"
+			>
+				<a :href="props.data.clone_url">{{ $t('clickToRepo') }}</a>
+			</h1>
+
+			<displayUrl
+				:deployedUrl="props.data.homepage"
+				v-if="props.data.homepage"
+				class="block md:hidden absolute bottom-3 right-3 group-hover:block"
+			/>
+		</section>
+
+		<section class="px-3 pb-3">
+			<section class="flex justify-between m-3">
+				<p>{{ props.data.name }}</p>
+				<UBadge variant="soft" size="md" :label="props.data.language" />
+			</section>
+
+			<UBadge
+				class="m-1 space-y-3"
+				:ui="{ rounded: 'rounded-full' }"
+				v-for="(childItem, index) in props.data.topics"
+				:key="index"
+				:label="childItem"
+			/>
+		</section>
+	</UCard>
+</template>
+
+<script lang="ts" setup>
+interface props {
+	data: {
+		clone_url: string
+		homepage: string
+		name: string
+		language: string
+		topics: string[]
+	}
+}
+const props = defineProps<props>()
+</script>
+
+<style></style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -2,47 +2,7 @@
 	<main
 		class="grid 2xl:grid-cols-4 xl:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-3 mx-auto"
 	>
-		<section
-			class="group rounded-3xl shadow m-3 hover:shadow-lg transition-all"
-			v-for="(item, index) in data?.data"
-			:key="index"
-		>
-			<section
-				class="relative text-white bg-slate-700 w-full min-h-48 p-5 rounded-3xl rounded-b-none"
-			>
-				<section class="flex justify-start items-center">
-					<UIcon name="i-grommet-icons:github" class="text-3xl mr-2" />
-					<p>{{ $t('githubRepo') }}</p>
-				</section>
-
-				<h1
-					class="text-2xl text-center my-10 transition-all group-hover:text-3xl"
-				>
-					<a :href="item.clone_url">{{ $t('clickToRepo') }}</a>
-				</h1>
-
-				<displayUrl
-					:deployedUrl="item.homepage"
-					v-if="item.homepage"
-					class="block md:hidden absolute bottom-3 right-3 group-hover:block"
-				/>
-			</section>
-
-			<section class="px-3 pb-3">
-				<section class="flex justify-between m-3">
-					<p>{{ item.name }}</p>
-					<UBadge variant="soft" size="md" :label="item.language" />
-				</section>
-
-				<UBadge
-					class="m-1 space-y-3"
-					:ui="{ rounded: 'rounded-full' }"
-					v-for="(childItem, index) in item.topics"
-					:key="index"
-					:label="childItem"
-				/>
-			</section>
-		</section>
+		<RepoCard v-for="(item, index) in data?.data" :key="index" :data="item" />
 	</main>
 </template>
 
