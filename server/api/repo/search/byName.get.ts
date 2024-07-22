265759d989b566703422fedd159cb89cc46603be
Nano <155299070+gaomingzhao666@users.noreply.github.com>
2024-07-22 21:09:21 +0800

    feature: add repos search by name functionality

---
+++ server/api/repo/search/byName.get.ts
@@ -0,0 +1,39 @@
+const getAllRepos = async () => {
+	const { data } = await $fetch<repoInfoGet>('/api/repo/info', {
+		method: 'GET',
+	})
+	return data
+}
+
+interface reposBody {
+	repoName: string
+}
+export default defineEventHandler(async (event) => {
+	const { repoName }: reposBody = getQuery(event)
+
+	if (!repoName)
+		return <errorType>{
+			status: false,
+			data: {
+				message: 'Repo name does not exist',
+			},
+		}
+
+	const allRepos = await getAllRepos()
+	const filteredRepos = allRepos.filter((item: any) =>
+		item.name.includes(repoName)
+	)
+
+	if (filteredRepos.length > 0) {
+		return <repoSearchByNameGet>{
+			status: true,
+			data: filteredRepos,
+		}
+	} else
+		return <errorType>{
+			status: false,
+			data: {
+				message: 'Repos not found',
+			},
+		}
+})
