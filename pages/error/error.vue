e2b44684a05707fe9b61bc17aced46bf9b5f7756
Nano <155299070+gaomingzhao666@users.noreply.github.com>
2024-09-23 14:28:42 +0800

    fix: error page ui

--- pages/error/error.vue
+++ pages/error/error.vue
@@ -1,14 +1,10 @@
-<!-- GPT4 generate -->
 <template>
-	<div class="error-container">
-		<section class="error-title">404</section>
-		<section class="error-description">
-			The page you're looking for can't be found.
-		</section>
-		<nuxt-link to="/">
-			<UButton class="error-button" size="xl">Go Home</UButton>
-		</nuxt-link>
-	</div>
+	<UContainer class="w-screen h-screen text-center mt-64">
+		<h1 class="text-5xl">Seems like something went wrong</h1>
+		<NuxtLink to="/">
+			<UButton variant="outline" size="xl" class="mt-10">Back to Home</UButton>
+		</NuxtLink>
+	</UContainer>
 </template>
 
 <script setup lang="ts">
@@ -17,24 +13,3 @@ useSeoMeta({
 	description: 'Error page.',
 })
 </script>
-
-<style scoped>
-.error-container {
-	text-align: center;
-	padding: 30px;
-	width: 380px;
-	background-color: #ffffff;
-	box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-	border-radius: 30px;
-}
-
-.error-title {
-	color: red;
-	font-size: 5rem;
-	margin: 0;
-}
-.error-description {
-	color: gray;
-	margin: 16px 0;
-}
-</style>
