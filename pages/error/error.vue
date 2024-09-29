fa26cfee947b2f6b873a707696e25a780df2736e
Nano <155299070+gaomingzhao666@users.noreply.github.com>
2024-09-29 15:07:20 +0800

    add: translations for error page

--- pages/error/error.vue
+++ pages/error/error.vue
@@ -1,8 +1,10 @@
 <template>
 	<UContainer class="w-screen h-screen text-center mt-64">
-		<h1 class="text-5xl">Seems like something went wrong</h1>
+		<h1 class="text-5xl">{{ $t('errorDesc') }}</h1>
 		<NuxtLink to="/">
-			<UButton variant="outline" size="xl" class="mt-10">Back to Home</UButton>
+			<UButton variant="outline" size="xl" class="mt-10">{{
+				$t('backToHome')
+			}}</UButton>
 		</NuxtLink>
 	</UContainer>
 </template>
