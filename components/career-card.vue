844366cb07125f0e9eaf7ee5178a1d96be8ec661
Nano <155299070+gaomingzhao666@users.noreply.github.com>
2024-09-30 21:42:40 +0800

    feat: career page

---
+++ components/career-card.vue
@@ -0,0 +1,51 @@
+<template>
+	<UCard
+		class="w-full rounded-3xl text-center space-x-2 first:ring-2 first:ring-green-400"
+	>
+		<section class="flex justify-between items-center">
+			<h2 class="font-semibold">
+				{{ props.data.startDate }} - {{ props.data.endDate }}
+			</h2>
+
+			<UBadge color="cyan" variant="soft" size="md">{{
+				props.data.employType
+			}}</UBadge>
+		</section>
+
+		<section class="flex flex-wrap md:flex-nowrap justify-around mt-2">
+			<section class="w-full">
+				<h1 class="font-bold text-xl text-start">{{ props.data.position }}</h1>
+
+				<div class="flex items-center justify-start text-gray-600">
+					<p>{{ props.data.company }}</p>
+					&nbsp; | &nbsp;
+					<p>{{ props.data.site }}</p>
+				</div>
+			</section>
+
+			<section>
+				<ul class="list-disc text-start ml-3">
+					<!-- responsible for -->
+					<li v-for="(item, index) in props.data.positionRoles" :key="index">
+						{{ item }}
+					</li>
+				</ul>
+			</section>
+		</section>
+	</UCard>
+</template>
+
+<script lang="ts" setup>
+interface Props {
+	data: {
+		startDate: string
+		endDate: string
+		employType: string
+		position: string
+		company: string
+		site: string
+		positionRoles: string[]
+	}
+}
+const props = defineProps<Props>()
+</script>
--- pages/index/career.vue
+++ pages/index/career.vue
@@ -1,15 +1,45 @@
 <template>
 	<UContainer>
-		<h1 class="text-center mt-52 text-3xl">
+		<h1 v-if="true" class="text-center mt-52 text-3xl">
 			Wanna be the first? Please
 			<NuxtLink to="/contact" class="text-blue-600">contact me</NuxtLink>
 		</h1>
+
+		<!-- I have no career since 2025, I probably start my career in winter of 2025 in japan, so temporarily hide this by set vif as false -->
+		<!-- title -->
+		<h1 class="text-center font-bold text-3xl my-3" v-if="false">
+			{{ $t('career') }}
+		</h1>
+
+		<!-- career contents -->
+		<section class="mt-5" v-if="false">
+			<CareerCard
+				v-for="(item, index) in [
+					{
+						startDate: '2022.01',
+						endDate: '2022.12',
+						employType: 'Full-time',
+						position: 'Software Engineer',
+						company: 'Xiaomi',
+						site: 'New york, US',
+						positionRoles: [
+							// role 1
+							'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+							// role 2
+							'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+						],
+					},
+				]"
+				:key="index"
+				:data="item"
+			/>
+		</section>
 	</UContainer>
 </template>
 
 <script lang="ts" setup>
 useSeoMeta({
-	title: 'Experience',
+	title: 'Career',
 	description: 'Discover my professional journey.',
 })
 </script>
