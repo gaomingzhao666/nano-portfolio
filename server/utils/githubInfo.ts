2dedd8380f24d068814055ec50022725a345fdee
Nano <gaomingzhao666@outlook.com>
2024-11-10 18:53:55 +0800

    chore: update `Github Fine-grained Token` for APIs

--- server/utils/githubInfo.ts
+++ server/utils/githubInfo.ts
@@ -1,5 +1,5 @@
 export const githubToken: string =
-	'github' +
-	'_pat_11BFA2Z7Q0Y1TrZbgUyzR0_9FoAy71QAZbV6vOGXHljQd0CPI9he9MkyCaVTD2A3NlZHQGHUX5iZuZK68Q'
+  'github' +
+  '_pat_11BFA2Z7Q0DMSkfsRIuX8n_7LRNIbJzkVWFGVkeiED48ql3y401nr4NsQ641Ht3J4AWOQH4HIAQxMgVC69'
 
 export const githubName: string = 'gaomingzhao666'
