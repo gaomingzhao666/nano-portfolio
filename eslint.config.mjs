ccfa0625b8dab49e40286b630afd0137722ca10c
Nano <gaomingzhao666@outlook.com>
2024-12-19 16:48:52 +0800

    chore: add custom rules to `eslint.confg.mjs`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt()
+export default withNuxt({
+  // Your custom configs here
+  rules: {
+    'vue/html-self-closing': 'off',
+    'vue/no-use-v-if-with-v-for': 'off',
+    'vue/no-use-v-else-with-v-for': 'off',
+  },
+})
 // Your custom configs here
