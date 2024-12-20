c518dad4c7609ec4317bbadc50b78cb2e5c92b9f
Nano <gaomingzhao666@outlook.com>
2024-12-20 22:21:42 +0800

    refactor: use `prettier` for new codes lint instead of `vue -official`

--- pages/index/contact.vue
+++ pages/index/contact.vue
@@ -1,58 +1,50 @@
 <template>
-	<UContainer class="mt-5">
-		<!-- tabs -->
-		<UTabs :items="items" v-model="selected" class="w-full">
-			<template #icon="{ item, selected }">
-				<UIcon
-					:name="item.icon"
-					class="w-4 h-4 flex-shrink-0 mr-2"
-					:class="[selected && 'text-primary-500 dark:text-primary-400']"
-				/>
-			</template>
-		</UTabs>
+  <UContainer class="mt-5">
+    <!-- tabs -->
+    <UTabs v-model="selected" :items="items" class="w-full" />
 
-		<!-- content -->
-		<ContactInfo v-if="selected === 0" />
-		<ContactForm v-if="selected === 1" />
-	</UContainer>
+    <!-- content -->
+    <ContactInfo v-if="selected === 0" />
+    <ContactForm v-if="selected === 1" />
+  </UContainer>
 </template>
 
 <script lang="ts" setup>
 useSeoMeta({
-	title: 'Contact me',
-	description: 'Find my contact information',
+  title: 'Contact me',
+  description: 'Find my contact information',
 })
 
 const items = [
-	{
-		label: 'Contact Info',
-		component: 'ContactInfo',
-		icon: 'i-heroicons:finger-print',
-	},
-	{
-		label: 'Contact Form',
-		component: 'ContactForm',
-		icon: 'i-heroicons:folder-plus',
-	},
+  {
+    label: 'Contact Info',
+    component: 'ContactInfo',
+    icon: 'i-heroicons:finger-print',
+  },
+  {
+    label: 'Contact Form',
+    component: 'ContactForm',
+    icon: 'i-heroicons:folder-plus',
+  },
 ]
 
 const route = useRoute()
 const router = useRouter()
 const selected = computed({
-	get() {
-		const index = items.findIndex((item) => item.label === route.query.tab)
-		if (index === -1) {
-			return 0
-		}
+  get() {
+    const index = items.findIndex((item) => item.label === route.query.tab)
+    if (index === -1) {
+      return 0
+    }
 
-		return index
-	},
-	set(value) {
-		// Hash is specified here to prevent the page from scrolling to the top
-		router.replace({
-			query: { tab: items[value].label },
-			hash: '#control-the-selected-index',
-		})
-	},
+    return index
+  },
+  set(value) {
+    // Hash is specified here to prevent the page from scrolling to the top
+    router.replace({
+      query: { tab: items[value].label },
+      hash: '#control-the-selected-index',
+    })
+  },
 })
 </script>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,28 +1,29 @@
 <template>
-	<main
-		class="grid 2xl:grid-cols-4 xl:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-3 mx-auto"
-	>
-		<RepoCard
-			v-for="(item, index) in data?.data"
-			:key="index"
-			:data="item"
-			v-if="isFilteredRepoExists"
-		/>
-		<h1 class="fixed top-1/3 left-1/2 transform font-bold text-3xl" v-else>
-			{{ noFilteredReposNotice }}
-		</h1>
-	</main>
+  <main
+    class="grid 2xl:grid-cols-4 xl:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-3 mx-auto"
+  >
+    <RepoCard
+      v-for="(item, index) in data?.data"
+      v-if="isFilteredRepoExists"
+      :key="index"
+      :data="item"
+    />
+
+    <h1 v-else class="fixed top-1/3 left-1/2 transform font-bold text-3xl">
+      {{ noFilteredReposNotice }}
+    </h1>
+  </main>
 </template>
 
 <script lang="ts" setup>
 useSeoMeta({
-	title: 'Index',
-	description:
-		'Explore a curated collection of GitHub repositories, organized by language and topic. Find the perfect project to inspire your next development endeavor.',
+  title: 'Index',
+  description:
+    'Explore a curated collection of GitHub repositories, organized by language and topic. Find the perfect project to inspire your next development endeavor.',
 })
 
 const { data, error } = await useFetch<repoInfoGet>('/api/repo/info', {
-	method: 'GET',
+  method: 'GET',
 })
 const router = useRouter()
 if (error.value) router.push({ name: 'error' })
@@ -30,52 +31,52 @@ if (error.value) router.push({ name: 'error' })
 const route = useRoute()
 
 // when search keyword changes, get filtered repos
-let isFilteredRepoExists: Ref<boolean> = ref(true)
-let noFilteredReposNotice: Ref<string> = ref('')
+const isFilteredRepoExists: Ref<boolean> = ref(true)
+const noFilteredReposNotice: Ref<string> = ref('')
 const getFilteredRepos = async (keywords: string) => {
-	const filteredReposByName = await $fetch<repoSearchByNameGet>(
-		`/api/repo/search/byName`,
-		{
-			method: 'GET',
-			query: {
-				repoName: keywords,
-			},
-		}
-	)
-	const filteredReposByTopics = await $fetch<repoSearchByTopicsGet>(
-		`/api/repo/search/byTopics`,
-		{
-			method: 'GET',
-			query: {
-				repoTopics: keywords,
-			},
-		}
-	)
+  const filteredReposByName = await $fetch<repoSearchByNameGet>(
+    `/api/repo/search/byName`,
+    {
+      method: 'GET',
+      query: {
+        repoName: keywords,
+      },
+    }
+  )
+  const filteredReposByTopics = await $fetch<repoSearchByTopicsGet>(
+    `/api/repo/search/byTopics`,
+    {
+      method: 'GET',
+      query: {
+        repoTopics: keywords,
+      },
+    }
+  )
 
-	let filteredRepos: any | string = []
-	filteredRepos = [
-		...(filteredReposByName.status ? filteredReposByName.data : []),
-		...(filteredReposByTopics.status ? filteredReposByTopics.data : []),
-	]
+  let filteredRepos: Array<repoInfoGet> | string = []
+  filteredRepos = [
+    ...(filteredReposByName.status ? filteredReposByName.data : []),
+    ...(filteredReposByTopics.status ? filteredReposByTopics.data : []),
+  ]
 
-	return {
-		status: filteredReposByName.status || filteredReposByTopics.status,
-		data: filteredRepos,
-	}
+  return {
+    status: filteredReposByName.status || filteredReposByTopics.status,
+    data: filteredRepos,
+  }
 }
 watchEffect(async () => {
-	if (route.query.searchKeywords) {
-		const filteredRepos = await getFilteredRepos(
-			route.query.searchKeywords.toString()
-		)
+  if (route.query.searchKeywords) {
+    const filteredRepos = await getFilteredRepos(
+      route.query.searchKeywords.toString()
+    )
 
-		if (filteredRepos.status) {
-			isFilteredRepoExists.value = true
-			data.value = filteredRepos
-		} else {
-			isFilteredRepoExists.value = false
-			noFilteredReposNotice.value = 'No repos found'
-		}
-	}
+    if (filteredRepos.status) {
+      isFilteredRepoExists.value = true
+      data.value = filteredRepos
+    } else {
+      isFilteredRepoExists.value = false
+      noFilteredReposNotice.value = 'No repos found'
+    }
+  }
 })
 </script>
