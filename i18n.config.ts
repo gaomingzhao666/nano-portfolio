9b14264c1791c53dcf52e7af6ea1367df63bd6fa
Nano <gaomingzhao666@outlook.com>
2025-01-14 16:04:00 +0800

    chore: modularize i18n `.json` files between purposes for `seo` and `ui`

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,14 +1,26 @@
 import en from '@/i18n/lang/en.json'
 import jp from '@/i18n/lang/jp.json'
 import cn from '@/i18n/lang/cn.json'
+import seoEN from '@/i18n/seo/en.json'
+import seoJP from '@/i18n/seo/jp.json'
+import seoCN from '@/i18n/seo/cn.json'
 
 export default defineI18nConfig(() => ({
   legacy: false,
   locale: 'en',
 
   messages: {
-    en,
-    jp,
-    cn,
+    en: {
+      ...en,
+      ...seoEN,
+    },
+    jp: {
+      ...jp,
+      ...seoJP,
+    },
+    cn: {
+      ...cn,
+      ...seoCN,
+    },
   },
 }))
