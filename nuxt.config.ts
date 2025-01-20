a3f2e00844f2f198c8b4823a9de6bb9166351956
Nano <gaomingzhao666@outlook.com>
2025-01-20 10:07:46 +0800

    chore: disable devtool

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   ssr: true,
 
   typescript: {
@@ -104,5 +104,5 @@ export default defineNuxtConfig({
     enabled: false,
   },
 
-  compatibilityDate: '2025-01-12',
+  compatibilityDate: '2025-01-20',
 })
