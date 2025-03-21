491575ef14254169f575b73edb35b8a8fa3d6bee
Nano <gaomingzhao666@outlook.com>
2025-03-21 21:30:11 +0900

    fix: use default `vueI18n` path to compatible with `@nuxtjs/i18n v9.3.x`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,108 +1,107 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: false },
-  ssr: true,
+	devtools: { enabled: false },
+	ssr: true,
 
-  typescript: {
-    typeCheck: true,
-  },
+	typescript: {
+		typeCheck: true,
+	},
 
-  app: {
-    head: {
-      templateParams: {
-        separator: '·',
-      },
-    },
-  },
+	app: {
+		head: {
+			templateParams: {
+				separator: '·',
+			},
+		},
+	},
 
-  alias: {
-    // in nuxt3, there have no src folder to storage codes that devs write, so the @ is using for root path
-    '@': import.meta.dirname,
-  },
+	alias: {
+		// in nuxt3, there have no src folder to storage codes that devs write, so the @ is using for root path
+		'@': import.meta.dirname,
+	},
 
-  modules: [
-    '@nuxt/test-utils/module',
-    'nuxt-mongoose',
-    '@pinia/nuxt',
-    '@nuxt/ui',
-    '@nuxtjs/i18n',
-    'nuxt-svgo',
-    '@nuxt/icon',
-    '@nuxt/eslint',
-    '@nuxtjs/seo',
-  ],
+	modules: [
+		'@nuxt/test-utils/module',
+		'nuxt-mongoose',
+		'@pinia/nuxt',
+		'@nuxt/ui',
+		'@nuxtjs/i18n',
+		'nuxt-svgo',
+		'@nuxt/icon',
+		'@nuxt/eslint',
+		'@nuxtjs/seo',
+	],
 
-  svgo: {
-    // enable custom icons from img file
-    componentPrefix: 'ico',
-  },
+	svgo: {
+		// enable custom icons from img file
+		componentPrefix: 'ico',
+	},
 
-  icon: {
-    // default: set automatically by nuxt, when deploy on a serverless platform such as vercel, set to local, if deploy on a server, set to remote
-    // here to spesifically set to local because all of the icon-sets are installed locally, that is also a recommend way
-    serverBundle: 'local',
-  },
+	icon: {
+		// default: set automatically by nuxt, when deploy on a serverless platform such as vercel, set to local, if deploy on a server, set to remote
+		// here to spesifically set to local because all of the icon-sets are installed locally, that is also a recommend way
+		serverBundle: 'local',
+	},
 
-  mongoose: {
-    uri: 'mongodb+srv://vercel-admin-user:AUK7HEtudWfWbJt3@gaomingzhao.skelvi3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    options: {},
-  },
+	mongoose: {
+		uri: 'mongodb+srv://vercel-admin-user:AUK7HEtudWfWbJt3@gaomingzhao.skelvi3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+		options: {},
+	},
 
-  // see https://i18n.nuxtjs.org/docs/guide/seo
-  i18n: {
-    vueI18n: './i18n.config.ts', // if you are using custom path, default
-    defaultLocale: 'en',
-    baseUrl: 'https://nano-portfolio-ruby.vercel.app',
+	// see https://i18n.nuxtjs.org/docs/guide/seo
+	i18n: {
+		defaultLocale: 'en',
+		baseUrl: 'https://nano-portfolio-ruby.vercel.app',
 
-    locales: [
-      {
-        code: 'en',
-        name: 'English',
-        language: 'en-US',
-      },
-      {
-        code: 'jp',
-        name: '日本語',
-        language: 'ja',
-      },
-      {
-        code: 'cn',
-        name: '简体中文',
-        language: 'zh-CN',
-      },
-    ],
-  },
+		locales: [
+			{
+				code: 'en',
+				name: 'English',
+				language: 'en-US',
+			},
+			{
+				code: 'jp',
+				name: '日本語',
+				language: 'ja',
+			},
+			{
+				code: 'cn',
+				name: '简体中文',
+				language: 'zh-CN',
+			},
+		],
+	},
 
-  // see https://nuxtseo.com/
-  site: {
-    title: 'Nano Portfolio',
-    url: 'https://nano-portfolio-ruby.vercel.app',
-  },
+	// see https://nuxtseo.com/
+	site: {
+		title: 'Nano Portfolio',
+		url: 'https://nano-portfolio-ruby.vercel.app',
+	},
 
-  schemaOrg: {
-    identity: 'Person',
-  },
+	schemaOrg: {
+		identity: 'Person',
+	},
 
-  seo: {
-    meta: {
-      description:
-        'A modern and open-source developers portfolio template that can automatically import Github profile and repository information, made by Vue/Nuxt ecosystem and Octokit APIs',
-      themeColor: [
-        { content: '#111827', media: '(prefers-color-scheme: dark)' },
-        { content: 'white', media: '(prefers-color-scheme: light)' },
-      ],
+	seo: {
+		meta: {
+			description:
+				'A modern and open-source developers portfolio template that can automatically import Github profile and repository information, made by Vue/Nuxt ecosystem and Octokit APIs',
+			themeColor: [
+				{ content: '#111827', media: '(prefers-color-scheme: dark)' },
+				{ content: 'white', media: '(prefers-color-scheme: light)' },
+			],
 
-      colorScheme: 'dark light',
-    },
-  },
+			colorScheme: 'dark light',
+		},
+	},
 
-  robots: {
-    // tell Google.com do not indexing /error pages
-    disallow: ['/error'],
-  },
-  ogImage: {
-    enabled: false,
-  },
+	robots: {
+		// tell Google.com do not indexing /error pages
+		disallow: ['/error'],
+	},
+	ogImage: {
+		enabled: false,
+	},
 
-  compatibilityDate: '2025-02-09',
+	compatibilityDate: '2025-02-09',
 })
