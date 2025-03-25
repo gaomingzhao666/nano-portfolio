282af0c4abfb4c41860a2f73ff2138edf44f38f5
Nano <gaomingzhao666@outlook.com>
2025-03-25 12:55:40 +0900

    fix: use `NuxtLink` to replace native `<a></a>` element to avoid https://github.com/gaomingzhao666/nano-portfolio/issues/4

--- components/displayUrl.vue
+++ components/displayUrl.vue
@@ -1,17 +1,18 @@
 <template>
-  <div>
-    <a
-      :href="props.deployedUrl"
-      class="hover:border-b-2 hover:border-b-sky-200"
-    >
-      {{ $t('clickToWebsite') }}
-    </a>
-  </div>
+	<div>
+		<NuxtLink
+			:to="props.deployedUrl"
+			class="hover:border-b-2 hover:border-b-sky-200"
+			target="_blank"
+		>
+			{{ $t('clickToWebsite') }}
+		</NuxtLink>
+	</div>
 </template>
 
 <script lang="ts" setup>
 interface props {
-  deployedUrl: string
+	deployedUrl: string
 }
 const props = defineProps<props>()
 </script>
