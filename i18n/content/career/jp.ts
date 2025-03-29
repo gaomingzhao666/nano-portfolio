b5b64e961b707a30498c89f688926ead0cc9376d
Nano <gaomingzhao666@outlook.com>
2025-03-29 21:28:33 +0900

    feat: make the career content localizable

---
+++ i18n/content/career/jp.ts
@@ -0,0 +1,44 @@
+export default [
+	{
+		startDate: '2022.01',
+		endDate: '2022.12',
+		employType: 'Full-time',
+		position: 'Software Engineer',
+		company: 'Xiaomi',
+		site: 'New york, US',
+		positionRoles: [
+			// role 1
+			'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+			// role 2
+			'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+		],
+	},
+	{
+		startDate: '2020.01',
+		endDate: '2021.12',
+		employType: 'Full-time',
+		position: 'Software Engineer',
+		company: 'Xiaomi',
+		site: 'New york, US',
+		positionRoles: [
+			// role 1
+			'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+			// role 2
+			'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+		],
+	},
+	{
+		startDate: '2018.01',
+		endDate: '2020.01',
+		employType: 'Full-time',
+		position: 'Software Engineer',
+		company: 'Xiaomi',
+		site: 'New york, US',
+		positionRoles: [
+			// role 1
+			'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+			// role 2
+			'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+		],
+	},
+]
--- pages/index/career.vue
+++ pages/index/career.vue
@@ -1,75 +1,61 @@
 <template>
-  <UContainer>
-    <!-- if you have no career, set it's display property to true -->
-    <!-- <h1 v-if="true" class="text-center mt-52 text-3xl">
+	<UContainer>
+		<!-- if you have no career, set it's display property to true -->
+		<!-- <h1 v-if="true" class="text-center mt-52 text-3xl">
 			Wanna be the first? Please
 			<NuxtLink to="/contact" class="text-blue-600">contact me</NuxtLink>
 		</h1> -->
 
-    <!-- title -->
-    <h1 class="text-center font-bold text-3xl my-3">
-      {{ $t('career') }}
-    </h1>
+		<!-- title -->
+		<h1 class="text-center font-bold text-3xl my-3">
+			{{ $t('career') }}
+		</h1>
 
-    <!-- career contents -->
-    <section class="mt-5 space-y-5">
-      <CareerCard
-        v-for="(item, index) in [
-          {
-            startDate: '2022.01',
-            endDate: '2022.12',
-            employType: 'Full-time',
-            position: 'Software Engineer',
-            company: 'Xiaomi',
-            site: 'New york, US',
-            positionRoles: [
-              // role 1
-              'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-              // role 2
-              'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-            ],
-          },
-          {
-            startDate: '2020.01',
-            endDate: '2021.12',
-            employType: 'Full-time',
-            position: 'Software Engineer',
-            company: 'Xiaomi',
-            site: 'New york, US',
-            positionRoles: [
-              // role 1
-              'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-              // role 2
-              'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-            ],
-          },
-          {
-            startDate: '2018.01',
-            endDate: '2020.01',
-            employType: 'Full-time',
-            position: 'Software Engineer',
-            company: 'Xiaomi',
-            site: 'New york, US',
-            positionRoles: [
-              // role 1
-              'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-              // role 2
-              'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-            ],
-          },
-        ]"
-        :key="index"
-        :data="item"
-      />
-    </section>
-  </UContainer>
+		<!-- career contents -->
+		<section class="mt-5 space-y-5">
+			<CareerCard v-for="(item, index) in []" :key="index" :data="item" />
+		</section>
+	</UContainer>
 </template>
 
 <script lang="ts" setup>
-const { t } = useI18n()
+import carrerContentEN from '@/i18n/content/career/en'
+import carrerContentJP from '@/i18n/content/career/jp'
+
+const { t, getBrowserLocale } = useI18n()
 
 useSeoMeta({
-  title: computed(() => t('career.title')),
-  description: computed(() => t('career.description')),
+	title: computed(() => t('career.title')),
+	description: computed(() => t('career.description')),
 })
+
+interface CareerContent {
+	startDate: string
+	endDate: string
+	employType: string
+	position: string
+	company: string
+	site: string
+	positionRoles: string[]
+}
+// make carrerContent reactive in order to switch localization reactively
+let carrerContent: CareerContent[] = reactive(carrerContentEN)
+const browserLocale = ref(getBrowserLocale())
+
+watch(
+	() => browserLocale.value,
+	(newValue) => {
+		switch (newValue) {
+			case 'en':
+				carrerContent = carrerContentEN
+				break
+			case 'jp':
+				carrerContent = carrerContentJP
+				break
+			default:
+				carrerContent = carrerContentEN
+				break
+		}
+	}
+)
 </script>
