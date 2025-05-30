9f7d5bba074a83cd0c2550f354cfd1c5e6ff18ad
Nano <gaomingzhao666@outlook.com>
2025-05-30 10:07:46 +0900

    chore: change returned value to boolean type in `verifyToken.ts`

--- server/utils/verifyToken.ts
+++ server/utils/verifyToken.ts
@@ -1,5 +1,12 @@
 import jwt from 'jsonwebtoken'
 
-export const verifyToken = (token: string) =>
-  // validate token
-  jwt.verify(token, 'secretByNano')
+export const verifyToken = (token: string) => {
+	try {
+		jwt.verify(token, 'secretByNano')
+	} catch (error) {
+		console.error('Token verification failed:', error)
+		return false
+	}
+
+	return true
+}
