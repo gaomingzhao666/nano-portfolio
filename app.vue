fa5a06ff1a1552e540c3d33155b84adef167d623
Nano <gaomingzhao666@outlook.com>
2025-06-05 22:23:56 +0900

    porting: add app wrapper

--- app.vue
+++ app.vue
@@ -1,28 +1,30 @@
 <template>
-  <div>
-    <NuxtLayout>
-      <NuxtLoadingIndicator />
+	<div>
+		<NuxtLayout>
+			<!-- <NuxtLoadingIndicator /> -->
 
-      <NuxtPage />
+			<UApp>
+				<NuxtPage />
+			</UApp>
 
-      <UNotifications />
-    </NuxtLayout>
-  </div>
+			<!-- <UNotifications /> -->
+		</NuxtLayout>
+	</div>
 </template>
 
 <script setup lang="ts">
 const { t } = useI18n()
 
 useSeoMeta({
-  title: computed(() => t('app.title')),
-  description: computed(() => t('app.description')),
+	title: computed(() => t('app.title')),
+	description: computed(() => t('app.description')),
 })
 
 useLoadingIndicator({
-  duration: 2000,
-  throttle: 200,
-  // This is how progress is calculated by default
-  estimatedProgress: (duration, elapsed) =>
-    (2 / Math.PI) * 100 * Math.atan(((elapsed / duration) * 100) / 50),
+	duration: 2000,
+	throttle: 200,
+	// This is how progress is calculated by default
+	estimatedProgress: (duration, elapsed) =>
+		(2 / Math.PI) * 100 * Math.atan(((elapsed / duration) * 100) / 50),
 })
 </script>
