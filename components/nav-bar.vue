37421a29cdd3b3fe26720dbd64f55dfa06ba7747
Nano <gaomingzhao666@outlook.com>
2025-06-05 22:25:10 +0900

    porting: https://ui.nuxt.com/getting-started/migration#renamed-components

--- components/nav-bar.vue
+++ components/nav-bar.vue
@@ -1,37 +1,38 @@
 <template>
-  <nav class="m-3 h-full md:w-[65px] lg:min-w-[200px] xl:min-w-[250px]">
-    <UVerticalNavigation
-      :links="[
-        {
-          label: $t('explore'),
-          icon: 'i-heroicons:cpu-chip',
-          to: '/',
-        },
-        {
-          label: $t('career'),
-          icon: 'i-heroicons:briefcase',
-          to: '/career',
-        },
-        {
-          label: $t('about'),
-          icon: 'i-heroicons:information-circle',
-          to: '/about',
-        },
-        {
-          label: $t('contact'),
-          icon: 'i-heroicons:globe-alt',
-          to: '/contact',
-        },
-        {
-          label: $t('comments'),
-          icon: 'i-heroicons:chat-bubble-oval-left-ellipsis',
-          to: '/comments',
-        },
-      ]"
-      :ui="{
-        padding: 'p-6',
-        rounded: 'rounded-lg',
-      }"
-    />
-  </nav>
+	<nav class="m-3 h-full md:w-[65px] lg:min-w-[200px] xl:min-w-[250px]">
+		<UNavigationMenu
+			:items="[
+				{
+					label: $t('explore'),
+					icon: 'i-heroicons:cpu-chip',
+					to: '/',
+				},
+				{
+					label: $t('career'),
+					icon: 'i-heroicons:briefcase',
+					to: '/career',
+				},
+				{
+					label: $t('about'),
+					icon: 'i-heroicons:information-circle',
+					to: '/about',
+				},
+				{
+					label: $t('contact'),
+					icon: 'i-heroicons:globe-alt',
+					to: '/contact',
+				},
+				{
+					label: $t('comments'),
+					icon: 'i-heroicons:chat-bubble-oval-left-ellipsis',
+					to: '/comments',
+				},
+			]"
+			:ui="{
+				padding: 'p-6',
+				rounded: 'rounded-lg',
+			}"
+			orientation="vertical"
+		/>
+	</nav>
 </template>
