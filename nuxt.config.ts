24118555976ed12e897513e1847d9f7c278112ea
Nano <gaomingzhao666@outlook.com>
2025-06-05 21:21:18 +0900

    chore: update to tailwindcss v4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: false },
+	css: ['./assets/styles/main.css'],
 	ssr: true,
 
 	typescript: {
