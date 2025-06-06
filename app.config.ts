58eb1e216cd37da95766b15671f2656f6cf90add
Nano <gaomingzhao666@outlook.com>
2025-06-06 21:55:47 +0900

    refactor: clear config code

--- app.config.ts
+++ app.config.ts
@@ -1,11 +1,3 @@
 export default defineAppConfig({
-  ui: {
-    primary: 'blue',
-    gray: 'cool',
-
-    notifications: {
-      // Show toasts at the top right of the screen
-      position: 'top-0 bottom-auto',
-    },
-  },
+	ui: {},
 })
