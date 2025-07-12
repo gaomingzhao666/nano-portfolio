1b211b61affc4ae2ff2721d8a42a40d9d7130fc7
Nano <gaomingzhao666@outlook.com>
2025-07-12 21:11:07 +0800

    update token

--- server/utils/githubInfo.ts
+++ server/utils/githubInfo.ts
@@ -1,5 +1,5 @@
 export const githubToken: string =
 	'github_' +
-	'pat_11BFA2Z7Q0mZXMXDwZgNq9_9TDd2P9eqEBq87GiwLdlJak8911PfTJ333ZG0M5L4XRQA3Q5LHErJKdBzhx'
+	'pat_11BFA2Z7Q0zO4vbutGaumK_42VdqImcxqhL1X7SwNaw7SRV3sMTP4vo3ExGtSQpEtSYQ5OTPLN06UpYlD9'
 
 export const githubName: string = 'gaomingzhao666'
