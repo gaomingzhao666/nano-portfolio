9295e1ab924cbfca630e814afeed0e8dba5b3508
Nano <gaomingzhao666@outlook.com>
2025-08-14 18:42:06 +0900

    add schedule link button

--- components/nav-bar.vue
+++ components/nav-bar.vue
@@ -1,37 +1,42 @@
 <template>
-  <nav class="m-3 h-full md:w-[65px] lg:min-w-[200px] xl:min-w-[250px]">
-    <UVerticalNavigation
-      :links="[
-        {
-          label: $t('explore'),
-          icon: 'i-heroicons:cpu-chip',
-          to: '/',
-        },
-        {
-          label: $t('career'),
-          icon: 'i-heroicons:briefcase',
-          to: '/career',
-        },
-        {
-          label: $t('about'),
-          icon: 'i-heroicons:information-circle',
-          to: '/about',
-        },
-        {
-          label: $t('contact'),
-          icon: 'i-heroicons:globe-alt',
-          to: '/contact',
-        },
-        {
-          label: $t('comments'),
-          icon: 'i-heroicons:chat-bubble-oval-left-ellipsis',
-          to: '/comments',
-        },
-      ]"
-      :ui="{
-        padding: 'p-6',
-        rounded: 'rounded-lg',
-      }"
-    />
-  </nav>
+	<nav class="m-3 h-full md:w-[65px] lg:min-w-[200px] xl:min-w-[250px]">
+		<UVerticalNavigation
+			:links="[
+				{
+					label: $t('explore'),
+					icon: 'i-heroicons:cpu-chip',
+					to: '/',
+				},
+				{
+					label: $t('career'),
+					icon: 'i-heroicons:briefcase',
+					to: '/career',
+				},
+				{
+					label: $t('schedule'),
+					icon: 'i-heroicons:calendar',
+					to: '/schedule',
+				},
+				{
+					label: $t('about'),
+					icon: 'i-heroicons:information-circle',
+					to: '/about',
+				},
+				{
+					label: $t('contact'),
+					icon: 'i-heroicons:globe-alt',
+					to: '/contact',
+				},
+				{
+					label: $t('comments'),
+					icon: 'i-heroicons:chat-bubble-oval-left-ellipsis',
+					to: '/comments',
+				},
+			]"
+			:ui="{
+				padding: 'p-6',
+				rounded: 'rounded-lg',
+			}"
+		/>
+	</nav>
 </template>
