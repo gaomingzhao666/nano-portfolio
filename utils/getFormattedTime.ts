bac1c6dae7f9b6ab4918f0d7b13d7c253e531b20
Nano <gaomingzhao666@outlook.com>
2025-08-14 14:52:57 +0900

    rename function

--- utils/getFormattedTime.ts
+++ utils/getFormattedTime.ts
@@ -1,6 +1,6 @@
 const date = new Date()
 
-export const formatDate = (date: Date) => {
+const formattedDateNow = (date: Date): string => {
 	const year = date.getFullYear()
 	const month = (date.getMonth() + 1).toString().padStart(2, '0') // Months are 0-indexed
 	const day = date.getDate().toString().padStart(2, '0')
@@ -8,5 +8,15 @@ export const formatDate = (date: Date) => {
 	// Format the date as YYYY-MM-DD
 	return `${year}-${month}-${day}`
 }
+const formattedDateNowPlusYears = (date: Date, years: number): string => {
+	const year = date.getFullYear() + years
+	const month = (date.getMonth() + 1).toString().padStart(2, '0') // Months are 0-indexed
+	const day = date.getDate().toString().padStart(2, '0')
 
-export const getFormattedTime = (): string => formatDate(date)
+	// Format the date as YYYY-MM-DD
+	return `${year}-${month}-${day}`
+}
+
+export const getFormattedDateNow = (): string => formattedDateNow(date)
+export const getFormattedDateNowPlusYears = (years: number): string =>
+	formattedDateNowPlusYears(date, years)
