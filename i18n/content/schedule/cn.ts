bc56c566940bcec230285782c2919aed3196a911
高 <m-kou@mldfp.com>
2026-02-28 22:28:11 +0900

    text format

--- i18n/content/schedule/cn.ts
+++ i18n/content/schedule/cn.ts
@@ -1,26 +1,26 @@
 export default [
   {
     reference:
-      "https://nano-blog-bice.vercel.app/post/2025/2025-08-13/annual-schedule-for-2025",
-    title: "计划 2025",
-    description: "2025 年度计划：未来职业规划与发展时间表。",
+      'https://nano-blog-bice.vercel.app/post/2025/2025-08-13/annual-schedule-for-2025',
+    title: '计划 2025',
+    description: '2025 年度计划：未来职业规划与发展时间表。',
     version: 1,
     list: [
       {
-        text: "在 5 个月内前往日本并找到 IT 工程师工作（包含签证审批时间）。",
+        text: '在 5 个月内前往日本并找到 IT 工程师工作（包含签证审批时间）。',
         isDone: false,
       },
-      { text: "顺利度过半年的正式员工试用期。", isDone: false },
+      { text: '顺利度过半年的正式员工试用期。', isDone: false },
       {
-        text: "准备 2026 年年中的 TOEIC 考试，准备时间约 9 个月，目标 800+。",
+        text: '准备 2026 年年中的 TOEIC 考试，准备时间约 9 个月，目标 800+。',
         isDone: false,
       },
       {
-        text: "若 2025 年第一场 JLPT N1 考试未通过，则参加第二次考试。",
+        text: '若 2025 年第一场 JLPT N1 考试未通过，则参加第二次考试。',
         isDone: true,
       },
       {
-        text: "提前准备 2026 年第二种电气工事士考试，参加第一场或第二场待定。",
+        text: '提前准备 2026 年第二种电气工事士考试，参加第一场或第二场待定。',
         isDone: false,
       },
     ],
--- i18n/content/schedule/en.ts
+++ i18n/content/schedule/en.ts
@@ -1,30 +1,30 @@
 export default [
   {
     reference:
-      "https://nano-blog-bice.vercel.app/post/2025/2025-08-13/annual-schedule-for-2025",
-    title: "Schedule for 2025",
+      'https://nano-blog-bice.vercel.app/post/2025/2025-08-13/annual-schedule-for-2025',
+    title: 'Schedule for 2025',
     description:
       "Nano's 2025 plan: career goals and preparations for the full year.",
     version: 1,
     list: [
       {
-        text: "Move to Japan and get an IT engineer job within 5 months, including visa waiting time.",
+        text: 'Move to Japan and get an IT engineer job within 5 months, including visa waiting time.',
         isDone: false,
       },
       {
-        text: "Complete the probation period.",
+        text: 'Complete the probation period.',
         isDone: false,
       },
       {
-        text: "Prepare for the TOEIC test.",
+        text: 'Prepare for the TOEIC test.',
         isDone: false,
       },
       {
-        text: "Take the second JLPT N1 test if I do not pass the first one in 2025.",
+        text: 'Take the second JLPT N1 test if I do not pass the first one in 2025.',
         isDone: true,
       },
       {
-        text: "Prepare for the `第二種電気工事士` test in 2026.",
+        text: 'Prepare for the `第二種電気工事士` test in 2026.',
         isDone: false,
       },
     ],
